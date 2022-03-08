aa4756a54acce95d18db35b8a1c9979f59228ace
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2022-03-07 22:33:01 -0400

    added

--- style.js
+++ style.js
@@ -1,15 +1,16 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const container = document.querySelector(".container");
-const content = document.querySelector('h1')
+const content = document.querySelector("h1");
 
 open.addEventListener("click", () => {
- console.log('opening')
-    container.classList.add("show-nav");
+  container.classList.add("show-nav");
 });
 
-close.addEventListener("click", () => container.classList.remove("show-nav"));
+close.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
 
-content.addEventListener('click', () => {
-    console.log('clicked')
-})
+content.addEventListener("click", () => {
+  console.log("clicked");
+});
